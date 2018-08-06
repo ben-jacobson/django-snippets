177972dad9d1ba0cc0ce0fcb10fd11d3b7a79e39
Ben Jacobson <ben_jacobson@live.com>
2018-08-06 22:14:09 +1000

    set up basic functional tests

---
+++ small_site_with_blog/functional_tests/tests.py
@@ -0,0 +1,17 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+
+class FunctionalTest(StaticLiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(MAX_WAIT)
+        self.browser.set_page_load_timeout(MAX_WAIT)
+
+    def tearDown(self):
+        self.browser.quit()
+        super().tearDown()
+
+class LayoutAndStylingTest(FunctionalTest):
+    def test_fail(self):
+        self.fail("functional test not complete")
