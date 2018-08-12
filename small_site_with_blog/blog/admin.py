159379ffca1793687c14e8a97ec1c72e7020f387
Ben Jacobson <ben_jacobson@live.com>
2018-08-12 20:36:11 +1000

    added to admin page the ability to date_created and date_published in list view, along with functional tests for them

--- small_site_with_blog/blog/admin.py
+++ small_site_with_blog/blog/admin.py
@@ -2,4 +2,29 @@ from django.contrib import admin
 from .models import Author, Entry
 
 admin.site.register(Author)
-admin.site.register(Entry, name="Entries")
+
+#admin.site.register(Entry, EntryAdmin) # instead of using this, you can use the decorator @admin, as seen below
+
+@admin.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    # list_display controls what you see when you view a list of objects, eg when you click Entries, you will see the title, and these fields below
+    list_display = ('title', 'author', 'date_created', 'date_published', )
+
+    #fields = ('author', 'title', 'date_created', 'date_published')     # when editing the entry, you can limit the types of fields you see like this. 
+    #exclude = ('author', ) # or alternatively you can have it include all fields, except for this exclude list.
+
+    # You can also change the fields so that they group together with fieldsets
+    '''fieldsets = (
+        (None, {
+            'fields': ('title', 'html_content',)
+        }),
+        ('dates', {
+            'classes': ('collapse',),       # this means by default it's collapsed
+            'fields': ('date_created', 'date_published'),
+        }),
+    )'''
+
+
+
+
+
--- small_site_with_blog/functional_tests/tests.py
+++ small_site_with_blog/functional_tests/tests.py
@@ -1,9 +1,11 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from datetime import date
+#from datetime import date
 from django.contrib.auth import get_user_model
 from blog.tests.base import create_test_author, create_test_blog_entry
+
+from datetime import datetime
 
 #from time import sleep
 
@@ -25,8 +27,14 @@ class FunctionalTest(StaticLiveServerTestCase):
         test_post_one.publish()
         test_post_two = create_test_blog_entry(title="Clickbait", author=test_author)
         test_post_two.publish()
-        create_test_blog_entry(title="Unpublished", author=test_author)
-        # don't publish the last one
+        create_test_blog_entry(title="Unpublished", author=test_author)  # don't publish the last one
+
+        # now create the super user for the tests concerning admin page
+        self.admin_page_user = 'admin'
+        self.admin_page_pass = 'blogpost'
+
+        User = get_user_model() 
+        User.objects.create_superuser(self.admin_page_user, self.test_email, self.admin_page_pass)          
 
     def tearDown(self):
         self.browser.quit()
@@ -63,21 +71,17 @@ class ViewBlogPostTest(FunctionalTest):
         # check that the page title is correct
         self.assertIn(blog_title, self.browser.find_element_by_tag_name('h1').text)
 
-class BlogPostingTest(FunctionalTest):
-    def test_can_post_blog_using_admin_page(self):
-        # Because we are using LiveServerTestCase, it won't have the super user already set, we'll need to set this.
-        admin_page_user = 'admin'
-        admin_page_pass = 'blogpost'
+class AdminPageTests(FunctionalTest):
+    def login_to_admin_page(self):
+        self.browser.find_element_by_id('id_username').send_keys(self.admin_page_user)
+        password_box = self.browser.find_element_by_id('id_password')
+        password_box.send_keys(self.admin_page_pass)
+        password_box.send_keys(Keys.ENTER)        
 
-        User = get_user_model() 
-        User.objects.create_superuser(admin_page_user, self.test_email, admin_page_pass)        
-
+    def test_can_post_blog_using_admin_page(self):      
         # Site owner goes to the admin page and logs in
         self.browser.get(self.live_server_url + '/admin')
-        self.browser.find_element_by_id('id_username').send_keys(admin_page_user)
-        password_box = self.browser.find_element_by_id('id_password')
-        password_box.send_keys(admin_page_pass)
-        password_box.send_keys(Keys.ENTER)
+        self.login_to_admin_page()
 
         # Site owner clicks blog entries
         self.browser.find_element_by_link_text('Entries').click()
@@ -107,5 +111,42 @@ class BlogPostingTest(FunctionalTest):
         # site owner goes back to entries and finds the new blog post there waiting for him
         self.assertEqual(test_title, self.browser.find_element_by_link_text(test_title).text)
 
+    def test_publish_button_in_admin_page(self):
+        # "Unpublished"
+        self.fail("finish the test")
 
-        
+    def test_list_of_blog_entries_show_published_status(self):
+        # Site owner goes to the admin page and logs in
+        self.browser.get(self.live_server_url + '/admin')
+        self.login_to_admin_page()
+
+        # Site owner clicks blog entries
+        self.browser.find_element_by_link_text('Entries').click()
+
+        # build a date string to test that the published date and time appear, eg 'Aug. 12, 2018'
+        string_from_time = datetime.strftime(datetime.now(), "%b. %d, %Y")  # this may break, unsure if the zero padding on the day matches what the admin page displays. If in doubt, just change this to just ', %Y'
+
+        # Site owner notices that there are 3 posts - 2 published, and 1 unpublished
+        date_published_fields = self.browser.find_elements_by_class_name('field-date_published')
+        self.assertEqual(3, len(date_published_fields))
+        self.assertEqual(date_published_fields[0].text, '-')
+        self.assertIn(string_from_time, date_published_fields[1].text)
+        self.assertIn(string_from_time, date_published_fields[2].text)
+
+    def test_list_of_blog_entries_show_date_created(self):
+        # Site owner goes to the admin page and logs in
+        self.browser.get(self.live_server_url + '/admin')
+        self.login_to_admin_page()
+
+        # Site owner clicks blog entries
+        self.browser.find_element_by_link_text('Entries').click()
+
+        # build a date string to test that the published date and time appear, eg 'Aug. 12, 2018'
+        string_from_time = datetime.strftime(datetime.now(), "%b. %d, %Y")  # this may break, unsure if the zero padding on the day matches what the admin page displays. If in doubt, just change this to just ', %Y'
+
+        # Site owner notices that there are 3 posts with date created listed on page
+        date_published_fields = self.browser.find_elements_by_class_name('field-date_created')
+        self.assertEqual(3, len(date_published_fields))
+        self.assertIn(string_from_time, date_published_fields[0].text)
+        self.assertIn(string_from_time, date_published_fields[1].text)
+        self.assertIn(string_from_time, date_published_fields[2].text)
