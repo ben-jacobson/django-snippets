8a67d42160ff5b9190d05b4d8f990aaf7c8fc5b4
Ben Jacobson <ben_jacobson@live.com>
2018-08-13 22:10:26 +1000

    refactored remaining tests to use reverse URL lookup

--- small_site_with_blog/blog/tests/test_views.py
+++ small_site_with_blog/blog/tests/test_views.py
@@ -20,22 +20,22 @@ class BlogViewTests(TestCase):
         response = self.client.get(reverse('home_page'))
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_list_of_blog_posts_uses_correct_template(self):
+    def test_blog_posts_list_uses_correct_template(self):
         response = self.client.get(reverse('blog_entries'))
         self.assertTemplateUsed(response, 'blog_entries.html')
 
-    def test_blog_entry_uses_correct_template(self):
-        response = self.client.get(reverse('blog_entries') + '1/')
+    def test_blog_post_uses_correct_template(self):
+        response = self.client.get(reverse('blog_post', kwargs={'blog_id': '1'}))
         self.assertTemplateUsed(response, 'blog_post.html')
 
     def test_blog_post_response_404_if_published_later_date(self):
-        response = self.client.get(reverse('blog_entries') + '2/')        
+        response = self.client.get(reverse('blog_post', kwargs={'blog_id': '2'}))        
         self.assertEqual(response.status_code, 404)
 
     def test_blog_post_response_404_if_not_published(self):
-        response = self.client.get(reverse('blog_entries') + '3/')        
+        response = self.client.get(reverse('blog_post', kwargs={'blog_id': '3'}))        
         self.assertEqual(response.status_code, 404)
 
     def test_blog_post_reqponse_404_if_doesnt_exist(self):        
-        response = self.client.get(reverse('blog_entries') + '100/')        
+        response = self.client.get(reverse('blog_post', kwargs={'blog_id': '100'}))        
         self.assertEqual(response.status_code, 404)        
