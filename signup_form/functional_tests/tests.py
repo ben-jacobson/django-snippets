0a4452fed86e6b7e5163a4b05e23dea7b1cb1360
Ben Jacobson <ben_jacobson@live.com>
2018-08-23 21:41:18 +1000

    created some unit tests for testing validation

--- signup_form/functional_tests/tests.py
+++ signup_form/functional_tests/tests.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 #from selenium.webdriver.common.keys import Keys
 
 from signup.models import Customer
+from signup.test_form_data import TEST_FORM_DATA
 
 MAX_WAIT = 10 # 10 second max wait
 
@@ -12,34 +13,31 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.browser.implicitly_wait(MAX_WAIT)
         self.browser.set_page_load_timeout(MAX_WAIT)
 
-        # some test data to include into the form and test against.
-        self.first_name = "Tester"
-        self.middle_name = "Selenium"
-        self.surname = "McTesterson"
-
     def tearDown(self):
         self.browser.quit()
         super().tearDown()
 
     def input_test_data_into_customer_form(self):
-        self.browser.get(self.live_server_url)
-        self.browser.find_element_by_id("id_title").send_keys("m")  # should default to Mr
-        self.browser.find_element_by_id("id_first_name").send_keys(self.first_name)
-        self.browser.find_element_by_id("id_middle_name").send_keys(self.middle_name)
-        self.browser.find_element_by_id("id_surname").send_keys(self.surname)
-        self.browser.find_element_by_id("id_gender").send_keys("Male")
-        self.browser.find_element_by_id("id_dob").send_keys("2018-08-31")
-        self.browser.find_element_by_id("id_email").send_keys("tester@mctestersonandco.com")
-        self.browser.find_element_by_id("id_phone").send_keys("6665468")
-        self.browser.find_element_by_id("id_mobile").send_keys("7771243")
-        self.browser.find_element_by_id("id_address_1").send_keys("Unit Test")
-        self.browser.find_element_by_id("id_address_2").send_keys("123 Test St")
-        self.browser.find_element_by_id("id_city").send_keys("Testville")
-        self.browser.find_element_by_id("id_state").send_keys("Test State")
-        self.browser.find_element_by_id("id_country").send_keys("au") # will default to Australia
+        self.browser.find_element_by_id("id_title").send_keys('m')  # should default to Mr
+        self.browser.find_element_by_id("id_first_name").send_keys(TEST_FORM_DATA['first_name'])
+        self.browser.find_element_by_id("id_middle_name").send_keys(TEST_FORM_DATA['middle_name'])
+        self.browser.find_element_by_id("id_surname").send_keys(TEST_FORM_DATA['surname'])
+        self.browser.find_element_by_id("id_gender").send_keys(TEST_FORM_DATA['gender'])
+        self.browser.find_element_by_id("id_dob").send_keys(TEST_FORM_DATA['dob'])
+        self.browser.find_element_by_id("id_email").send_keys(TEST_FORM_DATA['email'])
+        self.browser.find_element_by_id("id_phone").send_keys(TEST_FORM_DATA['phone'])
+        self.browser.find_element_by_id("id_mobile").send_keys(TEST_FORM_DATA['mobile'])
+        self.browser.find_element_by_id("id_address_1").send_keys(TEST_FORM_DATA['address_1'])
+        self.browser.find_element_by_id("id_address_2").send_keys(TEST_FORM_DATA['address_2'])
+        self.browser.find_element_by_id("id_city").send_keys(TEST_FORM_DATA['city'])
+        self.browser.find_element_by_id("id_state").send_keys(TEST_FORM_DATA['state'])
+        self.browser.find_element_by_id("id_country").send_keys('Au')
 
 class FormTest(FunctionalTest):
     def test_input_customer_data(self):
+        # user navigates to form page
+        self.browser.get(self.live_server_url)
+
         # data is entered into the form
         self.input_test_data_into_customer_form()
 
@@ -48,7 +46,7 @@ class FormTest(FunctionalTest):
      
         # check to see that the data is now in the database. TODO - figure out how to make this a model test somehow
         cust_data = Customer.objects.get(id=1)
-        self.assertEqual(cust_data.first_name, self.first_name)
-        self.assertEqual(cust_data.middle_name, self.middle_name)
-        self.assertEqual(cust_data.surname, self.surname)
+        self.assertEqual(cust_data.first_name, TEST_FORM_DATA['first_name'])
+        self.assertEqual(cust_data.middle_name, TEST_FORM_DATA['middle_name'])
+        self.assertEqual(cust_data.surname, TEST_FORM_DATA['surname'])
 
--- signup_form/signup/tests/tests.py
+++ signup_form/signup/tests/tests.py
@@ -1,7 +1,17 @@
 from django.test import TestCase
+from signup.forms import CustomerForm
 
-# Create your tests here.
+from signup.test_form_data import TEST_FORM_DATA
 
-class smoketest(TestCase):
-    def test_fail(self):
-        self.fail("Finish these tests")
+class FormTests(TestCase):
+    def test_form_validation(self):
+        # first attempt is to ensure form doesn't validate when it shouldn't
+        invalid_form_test_data = {'first_name': 'Test'}
+        first_test_form = CustomerForm(data=invalid_form_test_data)
+        first_test_form.full_clean()
+        self.assertFalse(first_test_form.is_valid(), msg='form.is_valid() should return false since we skipped mandatory fields')
+
+        # second attempt is to ensure form validates when it should
+        second_test_form = CustomerForm(data=TEST_FORM_DATA)
+        second_test_form.full_clean()
+        self.assertTrue(second_test_form.is_valid(), msg='form.is_valid() should return true since weve provided valid data')
